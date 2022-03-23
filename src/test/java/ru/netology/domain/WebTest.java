679e93ebd963a296c0df1b657e36914507926e2a
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-03-23 21:00:43 +0700

    initial commit

--- src/test/java/ru/netology/domain/WebTest.java
+++ src/test/java/ru/netology/domain/WebTest.java
@@ -3,6 +3,7 @@ package ru.netology.domain;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -31,5 +32,11 @@ public class WebTest {
     public void tearDown() {
         driver.quit();
         driver = null;
+    }
+
+    @Test
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999/");
+        System.out.println();
     }
 }
